ec975c327545eb3a5f9c145913ad46679e949218
yangjiePro <yangjie@jojoin.com>
2019-04-09 12:14:53 +0800

    clean_cache

--- app/controller/api/block/list.js
+++ app/controller/api/block/list.js
@@ -10,7 +10,10 @@ const model_block = appload('model/block')
 module.exports = async function(req, res)
 {
     try{
-        api.success(res, await model_block.getBlocks( req.query.last, req.query.limit))
+        let datas = await model_block.getBlocks( req.query.last, req.query.limit, req.query.clean_cache)
+        api.success(res, {
+            datas
+        })
     }catch(e){
         api.error(res, e.toString())
     }
--- app/model/block.js
+++ app/model/block.js
@@ -25,7 +25,24 @@ queryLastBlock()
 ////////////////////////////////////////////////
 
 
-let blockdatacaches = {} // 数据缓存
+let blockdatacache_maps = {} // 数据缓存
+
+// 从缓存里取数据
+function getBlocksFromCache(last, limit) {
+    // console.log(blockdatacache_maps)
+    let start = last - limit + 1
+    let results = []
+    for(let i=last; i>=start; i--){
+        let one = blockdatacache_maps[''+i]
+        // console.log(one)
+        if( ! one ){
+            return null
+        }
+        results.push( one )
+    }
+    // console.log("results", results.length)
+    return results
+}
 
 async function getBlocks(last, limit) {
     if( !last || !limit ){
@@ -35,23 +52,35 @@ async function getBlocks(last, limit) {
         limit -= last - lastblockdata.height
         last = lastblockdata.height
     }
-    // 检查缓存
-    let cachekey = last+'_'+limit
-    if( blockdatacaches[cachekey] ){
-        return blockdatacaches[cachekey]
+    // 从缓存取数据
+    let caches = getBlocksFromCache(last, limit)
+    if( caches ){
+        // console.log("getBlocksFromCache", last, limit)
+        return caches
     }
     try{
+        // console.log("await http_tool.json(config.miner_api_url", last, limit)
         let jsonobj = await http_tool.json(config.miner_api_url+"/query", {
             action: "blocks",
             start_height: parseInt(last) - parseInt(limit) + 1,
             end_height: last,
         })
-        // ok
-        blockdatacaches[cachekey] = jsonobj
+        let datas = jsonobj.datas
+        // ok 保存数据至缓存
+        let delete_keys = []
+        for(let i in datas){
+            let k = datas[i].height+''
+            delete_keys.push(k)
+            blockdatacache_maps[k] = datas[i]
+        }
+        // 定时清除缓存
         setTimeout(function(){
-            delete blockdatacaches[cachekey]
-        }, 1000*60*5)
-        return jsonobj
+            for(let k in delete_keys){
+                delete blockdatacache_maps[k]
+            }
+            // console.log("delete blocks cache key", delete_keys.join(','))
+        }, 1000*60*24*3) // 保存三天内的数据 288*3
+        return datas
     }catch(e){
         return []
     }
@@ -67,7 +96,12 @@ exports.getLastBlock = async function()
 }
 
 
-exports.getBlocks = getBlocks
+exports.getBlocks = async function(last, limit, clean_cache){
+    if(clean_cache){
+        blockdatacache_maps = {} // 清除缓存
+    }
+    return getBlocks(last, limit)
+}
 
 
 
