a025c801fca9c709dbf2f4ce1e745dda07e5b00a
yangjiePro <yangjie@jojoin.com>
2020-06-25 20:04:15 +0800

    up

--- language/en/header.js
+++ language/en/header.js
@@ -4,7 +4,7 @@ module.exports = {
     title_big: "Hacash Explorer",
     title_small: "for HacPool",
     
-    search_input_placeholder: "Block Height,Hash / Tx / Address / Diamond / Channel",
+    search_input_placeholder: "Height / Hash / Tx / Address / Diamond / Channel",
     search: "Search",
 
 }
