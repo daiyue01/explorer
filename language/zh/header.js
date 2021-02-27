ca6ddff9582ce3be3686954af0edac1cb0ff59b6
jojoin <yangjie@jojoin.com>
2021-02-27 14:00:12 +0800

    up

--- language/zh/header.js
+++ language/zh/header.js
@@ -1,8 +1,8 @@
 module.exports = {
 
 
-    title_big: "BLOCK.hacash.org",
-    title_small: "区块浏览器",
+    title_big: "Hacash 区块浏览器",
+    title_small: "",
     
     search_input_placeholder: "区块高度、哈希 / 交易哈希 / 账户地址 / 钻石 / 结算通道id",
     search: "查询",
