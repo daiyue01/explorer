5f6ea4400254d582df5928a016b403eb84a4bdaf
jojoin <yangjie@jojoin.com>
2021-06-12 20:31:54 +0800

    up

--- language/en/main.js
+++ language/en/main.js
@@ -28,8 +28,8 @@ module.exports = {
 
     trs_last: "Latest Transactions",
     trs_block_height: "Height",
-    trs_from: "From address",
-    trs_to: "To address",
+    trs_from: "From addr",
+    trs_to: "To addr",
     trs_amount: "Amount",
     trs_time: "Confirm time",
     trs_show_more: "Show More",
