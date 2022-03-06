adbef76dfd24fb12f0e2ee7f7c69c0a1089e9245
jojoin <yangjie@jojoin.com>
2022-03-06 18:18:51 +0800

    no_fee

--- app/controller/diaviews.js
+++ app/controller/diaviews.js
@@ -7,5 +7,7 @@ const viewer = appload('viewer')
 
 module.exports = function(req, res)
 {
-    viewer.render('diaviews', {}, req, res)
+    viewer.render('diaviews', {
+        show_fee: !(req.query.no_fee),
+    }, req, res)
 }
